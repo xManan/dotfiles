ab53ec79e7ee7bd2837d1bac4dfff4f8dc878a4c
manan chawla <mananchawla10@gmail.com>
2023-11-11 13:46:11 +0530

    dotfiles update

--- nvim/.config/nvim/lua/luasnip-config/init.lua
+++
@@ -1,9 +0,0 @@
-require("luasnip.loaders.from_vscode").lazy_load()
-
-local map = require'utils'.map
-local opts = { noremap = true, silent = true }
-
-map("i", "<C-k>", "<cmd>lua require'luasnip'.jump(1)<CR>", opts)
-map("s", "<C-k>", "<cmd>lua require'luasnip'.jump(1)<CR>", opts)
-map("i", "<C-j>", "<cmd>lua require'luasnip'.jump(-1)<CR>", opts)
-map("s", "<C-j>", "<cmd>lua require'luasnip'.jump(-1)<CR>", opts)
